11cc9f13946831f43dd26e44b17a9103b15684da
umer2saeed <umer2saeed@gmail.com>
2025-02-14 00:31:18 +0500

    Ecommerce Initial Commit

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../assets/images/logo.png';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import SliderOneImg from '../assets/images/banner-1.jpg';
+import SliderTwoImg from '../assets/images/banner-2.jpg';
+import ProductImg from '../assets/images/eight.jpg';
+
+const Home = () => {
+  return (
+    <>
+      <header className='shadow'>
+        <div className='bg-dark text-center py-2'>
+          <span className='text-white'>Your Fashion Partner</span>
+        </div>
+
+        <div className="container container-fluid">
+          <div className="row">
+            <div className="col-md-10 m-auto col-xl-12">
+              <Navbar expand="lg" className="">
+              <Navbar.Brand href="#"><img src={Logo} alt="" width={150} /></Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="ms-auto my-2 my-lg-0"navbarScroll>
+                  <Nav.Link href="#action1">Mens</Nav.Link>
+                  <Nav.Link href="#action2">Women</Nav.Link>
+                  <Nav.Link href="#action3">Kids</Nav.Link>
+                </Nav>
+                <div className="nav-right d-flex">
+                  <a href="" className='ms-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"></path></svg>
+                  </a>
+                  <a href="" className='mx-3'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="28" fill="currentColor" className="bi bi-bag" viewBox="0 0 16 16"><path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"></path></svg>
+                  </a>
+                </div>
+              </Navbar.Collapse>
+              </Navbar>
+            </div>
+          </div>
+        </div>
+        
+      </header>
+
+      <section className="hero-section">
+          <Swiper
+            spaceBetween={0}
+            slidesPerView={1}          
+            breakpoints={{
+              1024: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+              }
+            }}
+          >               
+            <SwiperSlide>
+                <div className="content" style={{ backgroundImage: `url(${SliderOneImg})` }}>                        
+                </div>                   
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className="content" style={{ backgroundImage: `url(${SliderTwoImg})` }}>                        
+                </div>
+            </SwiperSlide>                
+          </Swiper>
+      </section>
+
+      <section className='new-arrivals py-5'>
+          <div className="container">
+            <h2>New Arrivals</h2>
+            <div className="row mt-3">
+              <div className="col-md-3">
+                <div className="product card border-0">
+                  <div className="card-img">
+                    <img src={ ProductImg } className='w-100' alt="" />
+                  </div>
+                  <div className="card-body pt-3">
+                    <a href="#">Red Check Shirt for Men</a>
+                    <div className="price">
+                      $50 <span className='text-decoration-line-through'>$80</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
