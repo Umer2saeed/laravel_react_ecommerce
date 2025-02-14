1c4bb63048a2d8b60dd9c0ff119718596d8685f6
umer2saeed <umer2saeed@gmail.com>
2025-02-15 03:22:20 +0500

    Working on Home Page Design

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../assets/images/logo.png';
+import LogoBlack from '../assets/images/logo-white.png';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import SliderOneImg from '../assets/images/banner-1.jpg';
 import SliderTwoImg from '../assets/images/banner-2.jpg';
-import ProductImg from '../assets/images/eight.jpg';
+import LatestProducts from './common/LatestProducts';
+import FeaturedProducts from './common/FeaturedProducts';
+
 
 const Home = () => {
   return (
@@ -67,27 +70,61 @@ const Home = () => {
           </Swiper>
       </section>
 
-      <section className='new-arrivals py-5'>
-          <div className="container">
-            <h2>New Arrivals</h2>
-            <div className="row mt-3">
-              <div className="col-md-3">
-                <div className="product card border-0">
-                  <div className="card-img">
-                    <img src={ ProductImg } className='w-100' alt="" />
-                  </div>
-                  <div className="card-body pt-3">
-                    <a href="#">Red Check Shirt for Men</a>
-                    <div className="price">
-                      $50 <span className='text-decoration-line-through'>$80</span>
-                      
-                    </div>
-                  </div>
+      <LatestProducts/>
+
+      <FeaturedProducts/>
+
+      <footer className='py-5 text-white text-center'>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-3">
+                  <img src={ LogoBlack } width={150} alt="" />
+                  <p className='pt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, provident.</p>
+                </div>
+                <div className="col-md-3">
+                  <h3>Categories</h3>
+                  <ul>
+                    <li><a href="#">Kids</a></li>
+                    <li><a href="$">Women</a></li>
+                    <li><a href="$">Mens</a></li>
+                  </ul>
+                </div>
+                <div className="col-md-3">
+                <h3>Quick Links</h3>
+                  <ul>
+                    <li><a href="$">Login</a></li>
+                    <li><a href="$">Register</a></li>
+                  </ul>
+                </div>
+                <div className="col-md-3">
+                  <h3>Get in Touch</h3>
+                  <ul>
+                    <li>
+                      <a href="#">+92331342352</a>
+                    </li>
+                    <li><a href="#">info@example.com</a></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="row spotlight">
+                <div className="col-md-4 d-flex justify-content-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16"><path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5zm1.294 7.456A2 2 0 0 1 4.732 11h5.536a2 2 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456M12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"></path></svg>
+                  <h3 className='mx-2'>Free Delivery</h3>
+                </div>
+                <div className="col-md-4 d-flex justify-content-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cash" viewBox="0 0 16 16"><path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4"></path><path d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2z"></path></svg>
+                  <h3 className='mx-2'>Money Back Garantee</h3>      
+                </div>
+                <div className="col-md-4 d-flex justify-content-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-credit-card-2-back" viewBox="0 0 16 16"><path d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5z"></path><path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1m-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1"></path></svg>
+                  <h3 className='mx-2'>Secure Payments</h3>
                 </div>
               </div>
             </div>
-          </div>
-      </section>
+      </footer>
+
+     
     </>
   )
 }
